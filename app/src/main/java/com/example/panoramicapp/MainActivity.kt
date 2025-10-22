a00aaa508df867244eeb8def2c821d07ae446f18
francisco bascur <fr.bascur@duocuc.cl>
2025-10-22 20:04:03 -0300

    coment

--- app/src/main/java/com/example/panoramicapp/MainActivity.kt
+++ app/src/main/java/com/example/panoramicapp/MainActivity.kt
@@ -21,7 +21,7 @@ class MainActivity : ComponentActivity() {
             PanoramicAppTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
                     Greeting(
-                        name = "Android francisco b ",
+                        name = "Android francisco bascur ",
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
@@ -33,7 +33,7 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
     Text(
-        text = "Hello $name!",
+        text = "Helmmlo $name!",
         modifier = modifier
     )
 }
