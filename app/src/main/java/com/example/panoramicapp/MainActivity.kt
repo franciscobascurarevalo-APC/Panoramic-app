50f3c2147fbccd7e9db79899a7cfcb0318556402
francisco bascur <fr.bascur@duocuc.cl>
2025-10-22 20:07:54 -0300

    corrigiendo errores

--- app/src/main/java/com/example/panoramicapp/MainActivity.kt
+++ app/src/main/java/com/example/panoramicapp/MainActivity.kt
@@ -21,7 +21,7 @@ class MainActivity : ComponentActivity() {
             PanoramicAppTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
                     Greeting(
-                        name = "Android francisco bascur ",
+                        name = " francisco bascur arevalo ",
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
